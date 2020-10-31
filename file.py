7b8a3af5aa65da0275270ae3f09c0bb5511623f6
nav1996 <2017cs122@stu.ucsc.cmb.ac.lk>
2020-10-31 09:13:45 +0530

    file handling

---
+++ file.py
@@ -0,0 +1,10 @@
+import  os
+file =open("text.txt",'r')
+print(file.read())
+
+"""File handeling first 5 chars"""
+import  os
+file =open("text.txt",'r')
+print(file.read(5))
+
+"""File read lines"""
